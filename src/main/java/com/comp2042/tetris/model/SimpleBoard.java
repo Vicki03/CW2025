8ef87dd4bdc629f127a90f8a4e7aa6395b60bc9f
Vicki <vicki95213@gmail.com>
2025-10-17 20:51:34 +0800

    refactor: movd BrickRotator to model.rules

--- src/main/java/com/comp2042/tetris/model/SimpleBoard.java
+++ src/main/java/com/comp2042/tetris/model/SimpleBoard.java
@@ -4,6 +4,7 @@ import com.comp2042.*;
 import com.comp2042.logic.bricks.Brick;
 import com.comp2042.logic.bricks.BrickGenerator;
 import com.comp2042.logic.bricks.RandomBrickGenerator;
+import com.comp2042.tetris.model.rules.BrickRotator;
 
 import java.awt.*;
 
