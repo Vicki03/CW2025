38fc6cedf4d8afc273f6093d37ed7911bca6236c
Vicki <vicki95213@gmail.com>
2025-10-17 21:53:07 +0800

    refactor: moved NextShapeInfo to model

--- src/main/java/com/comp2042/tetris/model/rules/BrickRotator.java
+++ src/main/java/com/comp2042/tetris/model/rules/BrickRotator.java
@@ -1,6 +1,6 @@
 package com.comp2042.tetris.model.rules;
 
-import com.comp2042.NextShapeInfo;
+import com.comp2042.tetris.model.NextShapeInfo;
 import com.comp2042.logic.bricks.Brick;
 
 public class BrickRotator {
